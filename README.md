2086f2c7057f5c2a31b1a0293dfa48b85f19cd1a
Kidus-Nesibu <knesibu3@gmail.com>
2023-11-08 12:14:30 -0800

    task1

---
+++ README.md
@@ -0,0 +1,2 @@
+0x16. C - Simple Shell
+Authors: Kidus Nesibu, Abiy Beshah
---
+++ main.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main ()
+{
+	size_t len, getr;
+	char *buffer = NULL;
+	while (1)
+	{
+		printf("$");
+		getr = getline(&buffer, &len, stdin);
+		if (getr != -1)
+		{
+			printf("%s", buffer);
+		}
+		else
+		{
+			break;
+		}
+	}
+	free(buffer);
+	return (0);
+}
