b229bd683a628bb21ce30571fde560cc85f0f56d
Abiybeshah <abiyjem99@gmail.com>
2023-11-15 13:11:32 +0300

    shell.c function

--- shell.c
+++ shell.c
@@ -1,15 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/wait.h>
-#include <string.h>
-
-#define MAX_COMMAND_LENGTH 100
-
-void environment(void);
-void execute_command(char *cmd);
-void tokenize_command(char *cmd, char *argv[]);
-int str_cmp(const char *str1, const char *str2);
+#include "shell.h"
 
 /**
 * main - this is the main executing function
