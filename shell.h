50c559fa5d700b657d11aa855cd9f29bc977aede
Abiybeshah <abiyjem99@gmail.com>
2023-11-15 16:47:39 +0300

    #cisfun$

--- shell.h
+++ shell.h
@@ -9,7 +9,6 @@
 
 #define MAX_COMMAND_LENGTH 100
 
-void environment(void);
 void tokenize_command(char *cmd, char *argv[]);
 int str_cmp(const char *str1, const char *str2);
 void execute_command(char *cmd);
