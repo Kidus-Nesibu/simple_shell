00db69d20cb36bbc50e5af6373f42e4897b65365
Abiybeshah <abiyjem99@gmail.com>
2023-11-15 11:51:06 +0300

    shell function not betty comp

--- shell.c
+++ shell.c
@@ -11,76 +11,116 @@ void execute_command(char *cmd);
 void tokenize_command(char *cmd, char *argv[]);
 int str_cmp(const char *str1, const char *str2);
 
-int main() {
+/**
+* main - this is the main executing function
+* Return: Always 0
+*/
+int main(void)
+{
 	char *cmd = NULL;
 	size_t len = 0;
 
-	while (1) {
+	while (1)
+	{
 	printf("$ ");
 	fflush(stdout);
 
-	if (getline(&cmd, &len, stdin) == -1) {
+	if (getline(&cmd, &len, stdin) == -1)
+	{
 		printf("\nExiting the shell.\n");
 		break;
 	}
-	if (cmd[strlen(cmd) - 1] == '\n') {
+	if (cmd[strlen(cmd) - 1] == '\n')
 		cmd[strlen(cmd) - 1] = '\0';
-	}
+
 	if (str_cmp(cmd, "env") == 0)
-	{
 		environment();
-	}
+
 	if (str_cmp(cmd, "exit") == 0)
-	{
 		exit(0);
-	}
+
 	execute_command(cmd);
 	}
 
 	free(cmd);
 
-	return 0;
+	return (0);
 }
 
-void execute_command(char *cmd) {
+/**
+* execute_command - the function with the execute command
+* @cmd: the command to be executed
+* Return: void
+*/
+void execute_command(char *cmd)
+{
 	pid_t pid = fork();
 
-	if (pid < 0) {
-		perror("Fork failed");
+	if (pid < 0)
+	{
+	perror("Fork failed");
 		exit(EXIT_FAILURE);
-	} else if (pid == 0) {
+
+	}
+	else if (pid == 0)
+	{
 	char *argv[MAX_COMMAND_LENGTH];
+
 	tokenize_command(cmd, argv);
 	execve(argv[0], argv, NULL);
 	perror("Error");
 	exit(EXIT_FAILURE);
-	} else {
+	}
+	else
+	{
 		int status;
+
 		wait(&status);
 	}
 }
 
-void tokenize_command(char *cmd, char *argv[]) {
+/**
+* tokenize_command - this func tokenizes the command
+* @cmd: the command input
+* @argv: the array of commands
+* Return: void
+*/
+void tokenize_command(char *cmd, char *argv[])
+{
 	char *token;
 	int i = 0;
 
 	token = strtok(cmd, " ");
-	while (token != NULL) {
+	while (token != NULL)
+	{
 		argv[i++] = token;
 		token = strtok(NULL, " ");
 
 	argv[i] = NULL;
 	}
 }
-void environment()
+
+/**
+* environment - prints the current environment
+* @enviro: the input for environment
+* Return: void
+*/
+void environment(void)
 {
-	extern char** environ;
+	extern char **environ;
+
 	for (int i = 0; environ[i] != NULL; i++)
 	{
 		printf("%s\n", environ[i]);
 	}
 }
 
+/**
+* str_cmp - compares two strings
+* @str1: input for string 1
+* @str2: input for string 2
+* Return: 0 if they are the same otherwise if not
+*/
 int str_cmp(const char *str1, const char *str2)
 {
 	while (*str1 != '\0' && *str2 != '\0')
